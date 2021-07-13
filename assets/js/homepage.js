ee7d70c4d9f46285bcf1aa1b817a404556a2f086
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-07-13 00:43:55 -0500

    completed API call for the GitHub reespos for the GH user octocat, formatted the call using JSON

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,12 @@
+// Great way to get API information and reformat it via JSON into an array of objects.
 var getUserRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos");
+  var response = fetch("https://api.github.com/users/octocat/repos").then(
+    function (response) {
+      response.json().then(function (data) {
+        console.log(data);
+      });
+    }
+  );
 };
-
+console.log("outside");
 getUserRepos();
