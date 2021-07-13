f2f9a50b0e33ffeceaef64b36ef174110119cafd
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-07-13 00:53:27 -0500

    restructured the getUserRepos function so that the username can be interchanged easily

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,12 @@
-// Great way to get API information and reformat it via JSON into an array of objects.
-var getUserRepos = function () {
-  var response = fetch("https://api.github.com/users/octocat/repos").then(
-    function (response) {
-      response.json().then(function (data) {
-        console.log(data);
-      });
-    }
-  );
+var getUserRepos = function (user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
 };
-console.log("outside");
-getUserRepos();
+getUserRepos("mjh1985codeman");
