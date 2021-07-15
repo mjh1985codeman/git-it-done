49dcf9e89f0279428932e84161af1367edd4c8f2
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-07-14 23:05:38 -0500

    added warning on if a repo has more than 30 issues.

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 // reference to the HTML issues container
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -10,6 +11,11 @@ var getRepoIssues = function (repo) {
       response.json().then(function (data) {
         // pass response data to dom function
         displayIssues(data);
+
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       });
     } else {
       alert("There was a problem with your request!");
@@ -51,6 +57,19 @@ var displayIssues = function (issues) {
     // append to container
     issueEl.appendChild(typeEl);
   }
+};
+
+var displayWarning = function (repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
 };
 
 getRepoIssues("facebook/react");
